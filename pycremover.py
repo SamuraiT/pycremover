00f086c7a96ab8c64b761b5cc5a84bc4017f543d
SamuraiT <t.yasukawa01@gmail.com>
2013-12-14 01:26:24 +0900

    add pycremover and gitignore

---
+++ pycremover.py
@@ -0,0 +1,33 @@
+"""
+author: Yasukawa Tatsuro 
+license: license is same as MIT license
+contactinfo: https://github.com/SamuraiT
+"""
+import os
+class PycRemover(object):
+    """
+    PycRemover represents remover which removes all the pyc files
+    including sub-directory's pyc files
+    """
+    def __call__(self):
+        self.process()
+    
+    def process(self,cwd = os.getcwd()):
+        listdir = os.listdir(cwd)
+        self.remove(cwd,listdir)
+        for filename in listdir:
+            abs_filename = os.path.join(cwd,filename)
+            if os.path.isdir(abs_filename):
+                listdir = os.listdir( abs_filename )
+                self.remove(abs_filename,listdir)
+                self.process(abs_filename)
+                
+
+    def remove(self,cwd,listdir,extension='.pyc'):
+        for filename in listdir:
+            if filename.endswith(extension):
+                os.remove( os.path.join(cwd,filename) )
+    
+pycremover = PycRemover()
+pycremover()
+        
